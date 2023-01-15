a5785c90945690513e061f82436099a6135a361d
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-01-15 19:21:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3zad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string korisnik;
+            korisnik = Console.ReadLine();
+            string sVelika = korisnik.ToUpper();
+            string sMala = korisnik.ToLower();
+            string sPrvaTri = korisnik.Remove(3);
+            string sZadnjihPet = korisnik.Substring(korisnik.Length-5);
+            string s8_11 = korisnik.Substring(8, 11);
+
+            Console.WriteLine(sVelika+sMala+sPrvaTri+sZadnjihPet+s8_11);
+
+            Console.ReadKey();
+        }
+    }
+}
